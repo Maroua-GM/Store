712553da3874b5d0c1d671dce762825001d874d2
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-01-30 00:36:06 +0100

    second slide

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(".slider-one").not(".slick-intialized").slick({
 	autoplay: true,
 	autoplaySpeed: 3000,
 	dots: true,
-	prevArrow: ".site-slider.slider-btn.prev",
-	nextArrow: ".site-slider.slider-btn.next",
+	prevArrow: ".site-slider .slider-btn .prev",
+	nextArrow: ".site-slider .slider-btn .next",
+});
+// second slider
+
+$(".slider-two").not(".slick-intialized").slick({
+	prevArrow: ".site-slider-two .slider-btn .prev",
+	nextArrow: ".site-slider-two .slider-btn .next",
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	autoplaySpeed: 3000,
 });
