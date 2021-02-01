49861657cdc418a94857ad9df21f2a253dbd6f48
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-02-01 18:12:23 +0100

    third slider

--- js/main.js
+++ js/main.js
@@ -42,3 +42,36 @@ $(".slider-two")
 			},
 		],
 	});
+$(".slider-third")
+	.not(".slick-intialized")
+	.slick({
+		prevArrow: ".site-slider-third .slider-btn .prev",
+		nextArrow: ".site-slider-third .slider-btn .next",
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplaySpeed: 3000,
+		autoplayHoverPause: true,
+		responsive: [
+			{
+				breakpoint: 1300,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
